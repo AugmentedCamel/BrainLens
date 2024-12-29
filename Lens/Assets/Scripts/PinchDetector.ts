9245aac8bfae2aee774a0087dbcd8edbf99a10d4
Mika <mikameel@outlook.com>
2024-12-29 13:15:12 +0100

    Disable physics when holding the bubble

--- Lens/Assets/Scripts/PinchDetector.ts
+++ Lens/Assets/Scripts/PinchDetector.ts
@@ -6,19 +6,22 @@ export class ExampleHandScript extends BaseScriptComponent {
   @input objectPrefab:ObjectPrefab;
   //@input handFollower:SceneObject;
   
+  //physicsBody:BodyComponent 
+  
   bubbleStartPoint: vec3;
   latestBubble: SceneObject;
 
   private handInputData = SIK.HandInputData;
   private rightHand = this.handInputData.getHand('right');
   isPinching: boolean;
-  
+  isHolding: boolean;
  
   
   onAwake() {
     this.createEvent("UpdateEvent").bind(this.onUpdate.bind(this))
     this.rightHand.onPinchDown.add(this.onTriggerDown.bind(this))
     this.rightHand.onPinchUp.add(this.onTriggerUp.bind(this))
+    
     
     this.createEvent("TapEvent").bind(() => {
       // let body = this.debugBall.getComponent("Physics.BodyComponent")
@@ -38,18 +41,25 @@ export class ExampleHandScript extends BaseScriptComponent {
   
   onTriggerDown() {
     this.isPinching = true;
+    this.isHolding = true;
     print(`DOWN`);
     this.bubbleStartPoint = this.rightHand.thumbPad.position;
     this.latestBubble = this.objectPrefab.instantiate(this.sceneObject);
     this.latestBubble.getTransform().setWorldPosition(this.bubbleStartPoint)
     
-    // do physic things
+    // physic things
+    this.latestBubble.getComponent("Physics.BodyComponent").enabled = false;
+    
   }
   
   onTriggerUp() {
     this.isPinching = false;
     print(`RELEASED`);
+
+    this.latestBubble.getComponent("Physics.BodyComponent").enabled = true;
+    
     this.latestBubble = null;
+    
     //should set new target for smooth follower bubble that is on the same location and or the bubbl follows a bubbletarget
   }
 }
